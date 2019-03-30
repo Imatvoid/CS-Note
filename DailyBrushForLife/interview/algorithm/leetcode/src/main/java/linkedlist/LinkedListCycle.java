223ac8d13d08f00a2bd22854c2ebe4b3c638758f
yangxu <yangxucpp@gmail.com>
2019-03-30 17:55:44 +0800

    leetcode/src/main/java/easy/ReverseString.java leetcode/src/main/java/easy/ReverseString.md leetcode/src/main/java/linkedlist/LinkedListCycle.java eetcode/src/main/java/linkedlist/LinkedListCycle.md

---
+++ DailyBrushForLife/interview/algorithm/leetcode/src/main/java/linkedlist/LinkedListCycle.java
@@ -0,0 +1,19 @@
+package linkedlist;
+
+public class LinkedListCycle {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+
+        public boolean hasCycle(ListNode head) {
+            return false;
+        }
+    }
+
+}
