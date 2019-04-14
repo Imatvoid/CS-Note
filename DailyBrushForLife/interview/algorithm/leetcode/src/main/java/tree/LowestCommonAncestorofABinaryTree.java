4824fd27f3f7a3f7204794bc232fd5250d44d28c
yangxu <yangxucpp@gmail.com>
2019-04-14 22:53:24 +0800

    二叉树最低公共祖先

--- DailyBrushForLife/interview/algorithm/leetcode/src/main/java/tree/LowestCommonAncestorofABinaryTree.java
+++ DailyBrushForLife/interview/algorithm/leetcode/src/main/java/tree/LowestCommonAncestorofABinaryTree.java
@@ -10,9 +10,27 @@ public class LowestCommonAncestorofABinaryTree {
         TreeNode(int x) { val = x; }
     }
 
+
+    /**
+     * 最近公共祖先
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 
-
+        if(root == null || root == p || root ==q){
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if(left !=null && right !=null){
+            return root;
+        }
+        return left!=null?left:right;
+        // 一样的
+        //return right!=null?right:left;
 
     }
 }
