72fe5dd5ada6f38fef16cdee4a5f002d49461782
yangxu <yangxucpp@gmail.com>
2019-04-27 22:37:55 +0800

    leetcode

--- DailyBrushForLife/interview/algorithm/leetcode/src/main/java/temp.java
+++ DailyBrushForLife/interview/algorithm/leetcode/src/main/java/temp.java
@@ -60,9 +60,75 @@ public class temp {
     }
 
 
+    public int numDecodings(String s) {
+
+        if (s.isEmpty() || (s.length() >= 1 && s.charAt(0) == '0')) return 0;
+
+
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                dp[i + 1] = 0;
+            } else {
+                dp[i + 1] = dp[i];
+            }
+
+            if (i >= 1 && (s.charAt(i - 1) == '1' || (s.charAt(i - 1) == '2' && s.charAt(i) <= '6'))) {
+
+                dp[i + 1] += dp[i - 1];
+            }
+        }
+        return dp[n];
+
+    }
+
+
+    public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+        dp[0][0] = true;
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*' && dp[0][i-1]) {
+                dp[0][i+1] = true;
+            }
+        }
+        for (int i = 0 ; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == '.') {
+                    dp[i+1][j+1] = dp[i][j];
+                    continue;
+                }
+                if (p.charAt(j) == s.charAt(i)) {
+                    dp[i+1][j+1] = dp[i][j];
+                    continue;
+                }
+                if (p.charAt(j) == '*') {
+                    //a* only counts as empty
+                    if( dp[i+1][j-1]){
+                        dp[i+1][j+1] =true;
+                        continue;
+                    }
+
+                    //a* only counts as empty
+                    if (p.charAt(j-1) == s.charAt(i) || p.charAt(j-1) == '.') {
+                        dp[i+1][j+1] = dp[i][j+1];  //in this case, a* counts as multiple a
+
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+
 
     public static void main(String[] args) {
 
+        new temp().isMatch("a","ab*a");
 
 //        new temp().trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1});
 //
